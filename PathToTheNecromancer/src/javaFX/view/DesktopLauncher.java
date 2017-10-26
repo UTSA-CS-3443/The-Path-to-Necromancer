e6df600533bbe2119f2c4085cc02bc0eb7cfade0
HangedDragon96 <31324862+HangedDragon96@users.noreply.github.com>
2017-10-26 09:22:01 -0500

    New menu to check out

--- PathToTheNecromancer/src/javaFX/view/DesktopLauncher.java
+++ PathToTheNecromancer/src/javaFX/view/DesktopLauncher.java
@@ -8,7 +8,13 @@ import javaFX.model.Settings;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 import javafx.stage.Stage;
 
 /**
@@ -36,7 +42,12 @@ public class DesktopLauncher extends Application {
 	private Settings settings;
 	@Override
 	public void start(Stage primaryStage) {
-		
+		Image img = new Image("SabHunger.jpg");
+		BackgroundImage bgImg = new BackgroundImage(img, 
+		    BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT,
+		    BackgroundPosition.DEFAULT, 
+		    new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, false));
+		Background bkImg = new Background(bgImg);
 		theStage = primaryStage;
 		 settings = new Settings(100, false, false, 100, 100, false, false, Difficulty.EASY);
 		 primaryStage.setTitle("Path to the Necromancer");
@@ -44,6 +55,7 @@ public class DesktopLauncher extends Application {
 				FXMLLoader loader = new FXMLLoader();
 				loader.setLocation(DesktopLauncher.class.getResource("/MainMenu.fxml"));
 				this.mainMenu = loader.load();
+				this.mainMenu.setBackground(bkImg);
 				scene = new Scene(mainMenu,400,400);
 				DesktopLauncherController controller = loader.getController();
 				controller.setSettings(settings);
