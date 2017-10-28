b4c97ff1f0aef01844ff7fe2b189c2bf06748d06
enigma-phi <enigma-phi@users.noreply.github.com>
2017-10-28 00:17:04 -0500

    Update Player Class to include stats

--- PathToTheNecromancer/src/game/model/sprites/player/Player.java
+++ PathToTheNecromancer/src/game/model/sprites/player/Player.java
@@ -2,5 +2,40 @@ package game.model.sprites.player;
 
 import game.model.sprites.CharacterSprites;
 
-public abstract class Player extends CharacterSprites{
+/**
+ * The player class used throughout the game by the user.
+ * 
+ * @author enigma-phi
+ *
+ */
+public abstract class Player extends CharacterSprites {
+    /**
+     * The player's current level
+     */
+    private int level;
+    /**
+     * The player's current experience. After reaching a new level, the player's
+     * experience will be reset to zero.
+     */
+    private int experience;
+
+    /**
+     * The player's strength for physical attack and defense
+     */
+    private int strength;
+
+    /**
+     * The player's intelligence for magic attack and defense
+     */
+    private int intelligence;
+
+    /**
+     * The player's dexterity for intiative and reflex
+     */
+    private int dexterity;
+
+    /**
+     * The player's luck for critical hits
+     */
+    private int luck;
 }
